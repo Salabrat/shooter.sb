6e648d4ecc37b2b92740bb38e91962b52795477a
Salabrat <salabrat66@gmail.com>
2023-03-20 22:12:08 +0400

    GMspawnAI

--- Source/sbshooter/Private/SBGameModeBase.cpp
+++ Source/sbshooter/Private/SBGameModeBase.cpp
@@ -5,10 +5,43 @@
 #include "Player/SBBaseCharacter.h"
 #include "Player/SBPlayerController.h"
 #include "UI/SBGameHUD.h"
+#include "AIController.h"
 
 ASBGameModeBase::ASBGameModeBase()
 {
 	DefaultPawnClass = ASBBaseCharacter::StaticClass();
 	PlayerControllerClass = ASBPlayerController::StaticClass();
 	HUDClass = ASBGameHUD::StaticClass();
+}
+
+void ASBGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	SpawnBots();
+//	CreateTeamsInfo();
+//	CurrentRound = 1;
+//	StartRound();
+}
+
+UClass* ASBGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+	if (InController && InController->IsA<AAIController>())
+	{
+		return AIPawnClass;
+	}
+	return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASBGameModeBase::SpawnBots()
+{
+	if (!GetWorld()) return;
+	for (int32 i = 0; i < GameData.PlayersNum - 1; ++i)
+	{
+		FActorSpawnParameters SpawnInfo;
+		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		const auto SBAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+	RestartPlayer(SBAIController);
+	}
 }
--- Source/sbshooter/Public/SBGameModeBase.h
+++ Source/sbshooter/Public/SBGameModeBase.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "SBCoreTypes.h"
 #include "SBGameModeBase.generated.h"
 
+class AAIController;
 
 UCLASS()
 class SBSHOOTER_API ASBGameModeBase : public AGameModeBase
@@ -14,5 +16,21 @@ class SBSHOOTER_API ASBGameModeBase : public AGameModeBase
 	
 public:
 	ASBGameModeBase();
+
+	virtual void StartPlay() override;
+	virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+		TSubclassOf<AAIController> AIControllerClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+		TSubclassOf<APawn> AIPawnClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+		FGameData GameData;
+private:
+	void SpawnBots();
 
 };

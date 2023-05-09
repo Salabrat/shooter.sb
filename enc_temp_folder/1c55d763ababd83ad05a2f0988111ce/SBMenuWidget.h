964b9378b0e0855f46600a4c68cb7305c08075e6
Salabrat <salabrat66@gmail.com>
2023-05-10 01:25:11 +0300

    DesignUI/Fonts

---
+++ enc_temp_folder/1c55d763ababd83ad05a2f0988111ce/SBMenuWidget.h
@@ -0,0 +1,48 @@
+// Salabrat Game, All rights Reserved. SB4
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SBCoreTypes.h"
+#include "SBMenuWidget.generated.h"
+
+class UButton;
+class UHorizontalBox;
+class USBGameInstance;
+class USBLevelItemWidget;
+
+UCLASS()
+class SBSHOOTER_API USBMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+		UPROPERTY(meta = (BindWidget))
+		 UButton* StartGameButton;
+
+		UPROPERTY(meta = (BindWidget))
+			UButton* QuitGameButton;
+
+		UPROPERTY(meta = (BindWidget))
+			UHorizontalBox* LevelItemsBox;
+
+		UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+			TSubclassOf<UUserWidget> LevelItemWidgetClass;
+
+		virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY()
+		TArray<USBLevelItemWidget*> LevelItemWidgets;
+
+	UFUNCTION()
+	void OnStartGame();
+
+	UFUNCTION()
+		void OnQuitGame();
+
+	void InitLevelItems();
+	void OnLevelSelected(const FLevelData& Data);
+	USBGameInstance* GetSBGameInstance() const;
+};
